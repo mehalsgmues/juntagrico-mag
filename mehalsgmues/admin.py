7658e082539bc79da8a111fe1ad91ed1e92d6ff0
David Glenck <pascal.free@ihdg.ch>
2019-06-19 21:56:52 +0200

    show admin log in backend

--- mehalsgmues/admin.py
+++ mehalsgmues/admin.py
@@ -12,6 +12,10 @@ from django.urls import reverse
 from juntagrico.config import Config
 from django.utils.translation import gettext as _
 
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
 
 class ExtendedMemberAdminForm(MemberAdminForm):
     def __init__(self, *a, **k):
@@ -35,3 +39,64 @@ class ExtendedMemberAdmin(MemberAdmin):
 
 admin.site.unregister(Member)
 admin.site.register(Member, ExtendedMemberAdmin)
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'action_time'
+
+    readonly_fields = [f.name for f in LogEntry._meta.get_fields()]
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag_',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def action_flag_(self, obj):
+        flags = {
+            1: "Addition",
+            2: "Changed",
+            3: "Deleted",
+        }
+        return flags[obj.action_flag]
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = mark_safe(u'<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            ))
+        return link
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
