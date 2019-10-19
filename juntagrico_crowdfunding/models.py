07a9b8646ac62923b4899f1c21c0cb929fbd4279
David Glenck <pascal.free@ihdg.ch>
2019-10-19 18:48:11 +0200

    move crowdfunding to plugin

--- juntagrico_crowdfunding/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-from juntagrico_crowdfunding.entity.funding_project import *
-from juntagrico_crowdfunding.entity.fundable import *
-from juntagrico_crowdfunding.entity.funder import *
-from juntagrico_crowdfunding.entity.fund import *
--- mehalsgmues/juntagricoapp.py
+++ mehalsgmues/juntagricoapp.py
@@ -1,4 +1,3 @@
 from juntagrico.util import addons
 
-addons.config.register_admin_menu('cf/crowdfunding_admin_menu.html')
 addons.config.register_user_menu('mag_user_menu.html')
