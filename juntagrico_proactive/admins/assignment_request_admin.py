57f83dad16b1226ee9be651c6db87a01e0169a11
David Glenck <pascal.free@ihdg.ch>
2019-10-22 23:09:04 +0200

    fix admin view of assignment requests

--- juntagrico_proactive/admins/assignment_request_admin.py
+++ juntagrico_proactive/admins/assignment_request_admin.py
@@ -1,7 +1,23 @@
+from django.forms import ModelForm
 from juntagrico.admins import BaseAdmin
+
+from juntagrico_proactive.dao.assignmentrequestdao import AssignmentRequestDao
+from juntagrico_proactive.entity.assignment_request import AssignmentRequest
+
+
+class AssignmentRequestAdminForm(ModelForm):
+    class Meta:
+        model = AssignmentRequest
+        fields = '__all__'
+
+    def __init__(self, *a, **k):
+        super().__init__(*a, **k)
+        self.fields['approver'].queryset = AssignmentRequestDao.all_approvers()
 
 
 class AssignmentRequestAdmin(BaseAdmin):
+    form = AssignmentRequestAdminForm
+    raw_id_fields = ['member']
     list_display = ['member', 'amount', 'job_time', 'duration', 'activityarea',
                     'location', 'request_date', 'approver', 'status', ]
     readonly_fields = ('assignment',)
