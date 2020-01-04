9231a4918fe6be96cdcbe2d141e88352aa21c810
David Glenck <pascal.free@ihdg.ch>
2020-01-04 17:33:35 +0100

    fix wsgi

--- mehalsgmues/wsgi.py
+++ mehalsgmues/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for mehalsgmues project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 """
 
 import os
@@ -12,13 +10,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mehalsgmues.settings")
 
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
