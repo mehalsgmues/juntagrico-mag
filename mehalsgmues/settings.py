367aef14247c877e0897fc4bdeb7f266dcc13ab1
David Glenck <pascal.free@ihdg.ch>
2020-03-01 18:02:19 +0100

    keep secret secret

--- mehalsgmues/settings.py
+++ mehalsgmues/settings.py
@@ -8,9 +8,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8cd-j&jo=-#ecd1jjulp_s*7y$n4tad(0d_g)l=6@n^r8fg3rn'
+SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 
 DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'True') == 'True'
 
