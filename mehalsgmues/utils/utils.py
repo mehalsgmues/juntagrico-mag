71498fe4e1db05a03bc4dcfccf5b366552520fc8
David Glenck <pascal.free@ihdg.ch>
2020-12-18 19:47:32 +0100

    fix christmas bug

--- mehalsgmues/utils/utils.py
+++ mehalsgmues/utils/utils.py
@@ -18,7 +18,7 @@ def date_from_get(request, name, default, date_format="%Y-%m-%d"):
 def get_delivery_dates_of_month(delivery_weekday, relative_month):
     today = datetime.today()
     # get dates of next month if this month is half over.
-    month = today.month + int(today.day > 15) + relative_month
+    month = (today.month + int(today.day > 15) + relative_month - 1) % 12 + 1
     date = datetime(today.year, month, 1)
     next_delivery = date + timedelta(days=(delivery_weekday - 1 - date.weekday()) % 7)
     yield next_delivery
