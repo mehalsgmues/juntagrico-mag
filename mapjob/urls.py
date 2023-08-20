12c3a563ff41e36db8f30ce9ac52a30673efef88
pascalfree <pascal.free@ihdg.ch>
2023-08-20 16:18:45 +0200

    new flyer campaign

--- mapjob/urls.py
+++ mapjob/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from mapjob.views import job_map
+from mapjob.views import job_map, job_map_by_type, JobMapsListView
 
 app_name = 'mapjob'
+
 urlpatterns = [
     path('jobs', job_map, name='map'),
+    path('jobs/<int:type_id>', job_map_by_type, name='map_by_type'),
+    path('list', JobMapsListView.as_view(), name='list')
 ]
--- mapjob/views.py
+++ mapjob/views.py
@@ -1,5 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from juntagrico.entity.jobs import JobType
 
 from mapjob.models import MapJob
 from mapjob.utils import get_map_job_data
@@ -12,3 +16,18 @@ def job_map(request):
         'jobs': jobs,
         'map_job_data': get_map_job_data(jobs, request.user.member)
     })
+
+
+@login_required
+def job_map_by_type(request, type_id):
+    jobs = MapJob.objects.filter(type=type_id)
+    return render(request, 'mapjob/job_map.html', {
+        'jobs': jobs,
+        'map_job_data': get_map_job_data(jobs, request.user.member)
+    })
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class JobMapsListView(ListView):
+    queryset = JobType.objects.filter(recuringjob__mapjob__isnull=False)
+    template_name = "mapjob/mapjobtype_list.html"
