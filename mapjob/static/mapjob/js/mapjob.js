48eb35f821f82eb4e5753132058712bb0a8d326b
pascalfree <pascal.free@ihdg.ch>
2024-01-23 01:14:14 +0100

    mapjob: progressive dashboard [wip]

--- mapjob/static/mapjob/js/mapjob.js
+++ mapjob/static/mapjob/js/mapjob.js
@@ -14,40 +14,60 @@ function onEachFeature(feature, layer) {
 }
 
 $(document).ready(function () {
-    $('#map-container').append('<div id="job-map">')
-    let map = L.map('job-map', {fullscreenControl: true}).setView([48, 2], 11);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-        }).addTo(map);
-    let map_job_data_element = document.getElementById('map_job_data')
-    if (map_job_data_element) {
-        let map_job_data = JSON.parse(map_job_data_element.textContent)
-        let geo_json
-        if (map_job_data.data) {
-            geo_json = L.geoJSON(map_job_data['data'], {
-                onEachFeature: onEachFeature,
-                style: function(feature) {
-                    try{
-                        return {color: map_job_data.legend[feature.properties.status][1]}
-                    } catch {
-                        return {color: map_job_data.legend.default[1]}
-                    }
-                }
-            }).addTo(map)
-        } else {
-            geo_json = L.geoJSON(map_job_data, {style: {color: "#000000"}}).addTo(map)
-        }
-
-        map.fitBounds(geo_json.getBounds(), {padding: [100, 100]});
-
-        if (map_job_data.legend) {
-            L.control.Legend({
-                position: "topright",
-                title: "Legende",
-                legends: Object.values(map_job_data.legend).map((x) => ({label: x[0], type: "rectangle", color: x[1]}))
+    $('.map-container').each(function() {
+        let map = L.map(this, {fullscreenControl: true}).setView([48, 2], 11);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
             }).addTo(map);
+        let map_job_data_element = document.getElementById('map_job_data')
+        if (map_job_data_element) {
+            let map_job_data = JSON.parse(map_job_data_element.textContent)
+            let geo_json
+            if (Array.isArray(map_job_data)) {
+                geo_json = L.geoJSON(map_job_data, {style: {color: "#000000"}}).addTo(map)
+            } else {
+                if (!map_job_data.data) {
+                    map_job_data = map_job_data[this.dataset.jobs]
+                }
+                if (map_job_data) {
+                    geo_json = L.geoJSON(map_job_data['data'], {
+                        filter: function (feature) {
+                            if (feature.properties.visibility) {
+                                return feature.properties.visibility.filter(element => classes.includes('map-' + element));
+                            }
+                            return true
+                        },
+                        onEachFeature: onEachFeature,
+                        style: function (feature) {
+                            if (map_job_data.legend) {
+                                try {
+                                    return {color: map_job_data.legend[feature.properties.status][1]}
+                                } catch {
+                                    return {color: map_job_data.legend.default[1]}
+                                }
+                            }
+                            try {
+                                return {color: feature.properties.color}
+                            } catch {}
+                            return {}
+                        }
+                    }).addTo(map)
+                }
+            }
+
+            if (geo_json) {
+                map.fitBounds(geo_json.getBounds(), {padding: [100, 100]});
+            }
+
+            if (map_job_data && map_job_data.legend) {
+                L.control.Legend({
+                    position: "topright",
+                    title: "Legende",
+                    legends: Object.values(map_job_data.legend).map((x) => ({label: x[0], type: "rectangle", color: x[1]}))
+                }).addTo(map);
+            }
         }
-    }
+    })
 })
--- mapjob/views.py
+++ mapjob/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Q, F
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.utils.safestring import mark_safe
@@ -38,15 +39,16 @@ class JobMapsListView(ListView):
 
 @login_required
 def member_dashboard(request):
-    jobs = MapJob.objects.filter(assignment__member=request.user.member).distinct()
+    # jobs of member
+    own_jobs = MapJob.objects.filter(assignment__member=request.user.member).distinct()
 
-    map_job_data = []
-    for map_job in jobs:
+    own_job_geo = []
+    for map_job in own_jobs:
         geo = map_job.geo_area
         geo['properties'].update({
-            'status': map_job.progress
+            'status': map_job.progress,
         })
-        map_job_data.append(geo)
+        own_job_geo.append(geo)
 
     colors = {
         MapJob.Progress.OPEN: "#ff0000",
@@ -60,12 +62,43 @@ def member_dashboard(request):
     }
     legend["default"] = [_("Unbekannt"), "#000000"]
 
+    # available map jobs
+    available_jobs = MapJob.objects.annotate(occupied_count=Count('assignment')).filter(
+        Q(infinite_slots=True) | Q(occupied_count__lt=F('slots'))
+    )
+
+    available_job_geo = []
+    for map_job in available_jobs:
+        geo = map_job.geo_area
+        geo['properties'].update({
+            'color': "#ff0000",
+        })
+        available_job_geo.append(geo)
+
+    # pickup form
+    pickup_form = None
+    if own_jobs.filter(progress__in=[MapJob.Progress.OPEN, MapJob.Progress.NEED_MORE]).exists():
+        pickup_form = PickupLocationForm
+
+    # return form
+    return_form = None
+    if own_jobs.exclude(progress__in=[MapJob.Progress.OPEN, MapJob.Progress.RETURNED]).exists():
+        return_form = PickupLocationForm
+
     return render(request, 'mapjob/dashboard.html', {
-        'jobs': jobs,
+        'own_jobs': own_jobs,
+        'available_jobs': available_jobs,
         'map_job_data': {
-            'data': map_job_data,
-            'legend': legend
+            'reserved': {
+                'data': own_job_geo,
+                'legend': legend
+            },
+            'available': {
+                'data': available_job_geo
+            }
         },
+        'pickup_form': pickup_form,
+        'return_form': return_form,
     })
 
 
