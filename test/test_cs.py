e6a25153de5c8443e13a43c6e6fafe0d4a0fdde6
pascalfree <pascal.free@ihdg.ch>
2026-02-20 15:38:54 +0100

    fix test and put code before link

--- test/test_cs.py
+++ test/test_cs.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from juntagrico.models import Member, Share, Subscription
 from juntagrico.tests import JuntagricoTestCase
 
+from antispam.models import EmailToken
+
 
 class CreateSubscriptionTests(JuntagricoTestCase):
 
@@ -13,7 +15,8 @@ class CreateSubscriptionTests(JuntagricoTestCase):
         self.client.force_login(self.member.user)
         user = auth.get_user(self.client)
         assert user.is_authenticated
-        self.client.get('/my/signup/')
+        email_token = EmailToken.objects.create()
+        self.client.get(reverse('member-create', args=[email_token.uid, email_token.token]))
         self.assertEqual(str(auth.get_user(self.client)), 'AnonymousUser')
 
     def testRedirect(self):
@@ -59,6 +62,14 @@ class CreateSubscriptionTests(JuntagricoTestCase):
         self.member.user.user_permissions.add(
             Permission.objects.get(codename='notified_on_share_creation'))
         self.member.user.save()
+        member_email = 'test@user.com'
+        response = self.client.post(reverse('pre-signup'), {'email': member_email,})
+        email_token = EmailToken.objects.first()
+        self.assertRedirects(response, reverse('confirm-email', args=[email_token.uid]))
+        response = self.client.post(reverse('confirm-email', args=[email_token.uid]), {
+            'token': email_token.token
+        })
+        self.assertRedirects(response, reverse('member-create', args=[email_token.uid, email_token.token]))
         new_member_data = {
             'last_name': 'Last Name',
             'first_name': 'First Name',
@@ -67,11 +78,10 @@ class CreateSubscriptionTests(JuntagricoTestCase):
             'addr_location': 'Zurich',
             'phone': '044',
             'mobile_phone': '',
-            'email': 'test@user.com',
             'birthday': '',
             'agb': 'on'
         }
-        response = self.client.post(reverse('signup'), new_member_data)
+        response = self.client.post(reverse('member-create', args=[email_token.uid, email_token.token]), new_member_data)
         self.assertRedirects(response, reverse('cs-subscription'))
         response = self.client.post(
             reverse('cs-subscription'),
@@ -107,7 +117,7 @@ class CreateSubscriptionTests(JuntagricoTestCase):
         # confirm summary
         response = self.client.post(reverse('cs-summary'))
         self.assertRedirects(response, reverse('welcome-with-sub'))
-        self.assertEqual(Member.objects.filter(email=new_member_data['email']).count(), 1)
-        self.assertEqual(Share.objects.filter(member__email=new_member_data['email']).count(), 1)
-        self.assertEqual(Subscription.objects.filter(primary_member__email=new_member_data['email']).count(), 1)
-        self.assertEqual(len(mail.outbox), 5)  # welcome mail, share mail & 3 admin notifications
+        self.assertEqual(Member.objects.filter(email=member_email).count(), 1)
+        self.assertEqual(Share.objects.filter(member__email=member_email).count(), 1)
+        self.assertEqual(Subscription.objects.filter(primary_member__email=member_email).count(), 1)
+        self.assertEqual(len(mail.outbox), 6)  # token mail, welcome mail, share mail & 3 admin notifications
